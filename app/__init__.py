5a4c597f5771a0cf1dc47a45a0a1e799d57c841e
Hanger Kim <hangerkimjr@gmail.com>
2019-08-28 19:03:39 +0900

    Flask-Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 
 
 def create_app(test_config=None):
@@ -21,6 +22,8 @@ def create_app(test_config=None):
 
     from app import filters, views
     from app.models import db
+
+    migrate = Migrate(app, db)
 
     app.register_blueprint(filters.bp)
     app.register_blueprint(views.bp)
