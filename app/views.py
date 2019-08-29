909620e72095f02d574d8d1246afce3fc39f90d3
Hanger Kim <hangerkimjr@gmail.com>
2019-08-29 18:41:05 +0900

    Cache issues on static files

--- app/views.py
+++ app/views.py
@@ -12,6 +12,20 @@ from app.models import db, Candidate, Result
 bp = Blueprint('views', __name__, url_prefix='/')
 
 
+@bp.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename is not None:
+            path = os.path.join(bp.root_path, endpoint, filename)
+            values['ts'] = int(os.stat(path).st_mtime)
+    return url_for(endpoint, **values)
+
+
 @bp.route('/')
 def home():
     return render_template('home.html')
